d7b5fcd12f2d69ee90e8995c52ecdcdbe8597289
Meet <meet.mukadam@somaiya.edu>
2018-01-23 11:32:40 +0530

    Cross entropy cost working.

--- Neural_Net.py
+++ Neural_Net.py
@@ -1,18 +1,11 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import mnist_loader
-import scipy.special
 import random
 
+#Load training, validation & testing data from MNIST dataset
+train_data, valid_data, test_data = mnist_loader.load_data_wrapper()
 
-# In[2]:
-
-
+#Create a new network
 class Network:
     def __init__(self,neurons):
         self.layers = len(neurons)
@@ -25,33 +18,21 @@ class Network:
             cols = neurons[i-1]
             
             #Creates a weight numpy array with dimension = dims
-            #Initializes them with random normal distributed value between 0-1
+            #Initializes them with random normal distributed value between -1 - +1
             
             #layer_weight = np.zeros((rows,cols))
             layer_weight = np.random.randn(rows,cols)*np.sqrt(6.0/(rows+cols))
             self.weights.append(layer_weight)
+            
             #layer_bias = np.random.randn(rows,1)
             layer_bias = np.zeros((rows,1))
             self.biases.append(layer_bias)
 
-
-# In[3]:
-
-
-train_data, valid_data, test_data = mnist_loader.load_data_wrapper()
-
-
-# In[4]:
-
-
+#Sigmoid function
 def sigmoid(z):
-    
     return (1.0/(1.0+np.exp(-z)))
 
-
-# In[5]:
-
-
+#
 def feedforward(network,X):
     layer_activation_list = []
     weighted_sum_list = []
@@ -69,12 +50,8 @@ def feedforward(network,X):
         layer_activation_list.append(a)
         
     return (layer_activation_list,weighted_sum_list)
-    
 
-
-# In[6]: Checking Accuracy
-
-
+#
 def test(network,data):
     
     activation_list,_ = feedforward(network,data[0])
@@ -85,28 +62,21 @@ def test(network,data):
     
     return accuracy
 
-
-# In[7]:
-
-
+#
 def cost_gradient(activation,target):
-    #print "Cost Gradients =",activation-target
-    
-    return (activation - target)
 
+    #Quadratic cost gradient
+    #return (activation - target)
 
-# In[8]:
-
-
+    #Cross Entropy cost gradient
+    return np.array(np.nan_to_num(-(target/activation)+(1-target)/(1-activation)))
+#
 def sigmoid_derivative(z):
+
     #print "Sigmoid_Derivatives =",sigmoid(z)*(1-sigmoid(z))
-    
     return sigmoid(z)*(1-sigmoid(z))
 
-
-# In[9]:
-
-
+#
 def update_weights(network,activation_list,delta_list,alpha,X):
     
     for i in range(network.layers-1):
@@ -124,14 +94,10 @@ def update_weights(network,activation_list,delta_list,alpha,X):
         network.biases[i] -= alpha*dcdb
         
         #print "Weights of {0} ".format(i),network.weights[i]
-        
         #print "**** Max_After : {0} , Min_After : {1} ".format(np.amax(network.weights[i]),np.amin(network.weights[i]))
     return
 
-
-# In[10]:
-
-
+#Backpropogation of error
 def errorback(network,last_delta,weighted_list):
     
     delta_list = []
@@ -143,61 +109,59 @@ def errorback(network,last_delta,weighted_list):
         
     return delta_list
 
-
-# In[11]:
-
-
+#Stochastic GD
 def gradient_descent(network,train_data,valid_data,alpha,epochs,batch_size):
     
     X = train_data[0]
     target = train_data[1]
+
     alpha = alpha/float(batch_size)
 
     for e in range(epochs):
 
+        #Cannot extract coloums from numpy array so take transpose
         X_trans = X.transpose()
         Y_trans = target.transpose()
 
+        #Extract rows (previously coloums) from input and target and pack them in a list
         train_list = [[X_trans[i],Y_trans[i]] for i in range(0,X.shape[1])]
 
+        #Random batch_size dataset for every epoch
         random.shuffle(train_list)
 
         for b in range(0,len(train_list),batch_size):
 
             train_batch = train_list[b:b+batch_size]
 
+            #Unpack the list and store input and target as different arrays
             Xbatch = np.array([t[0] for t in train_batch]).transpose()
             Ybatch = np.array([t[1] for t in train_batch]).transpose() 
 
+            #Generate activations (a) and weighted list (z) for every layer and neuron 
             activation_list, weighted_list = feedforward(network,Xbatch)
 
+            #Calculate error at the final layer
             last_layer_delta = cost_gradient(activation_list[-1],Ybatch)*sigmoid_derivative(weighted_list[-1])
 
+            #Back propogate the error to all remaining layers
             final_delta_list = errorback(network,last_layer_delta,weighted_list)
+
+            #Reverse a list
             final_delta_list = final_delta_list[::-1]
 
+            #Update the old weights
             update_weights(network,activation_list,final_delta_list,alpha,Xbatch)
         
         accuracy = test(network,valid_data)
-        print "Accuracy after Epoch [",e,"]",accuracy
+        print "Accuracy after Epoch {0}: {1}".format((e+1),accuracy)
         
     return
 
-
-# In[12]:
-
-
-my_net = Network([784, 30, 10])
-
 #temp = test_data[1]
 #print (sum(temp == 0).astype(np.float32))
-#Reverse a list
-#final_delta_list = final_delta_list[::-1]
-#print final_delta_list
+my_net = Network([784, 30, 10])
 epochs = 50
 alpha = 0.5
 batch_size = 100
-#update_weights(my_net,activation_list,final_delta_list,alpha,X)
-#print my_net.weights
 gradient_descent(my_net,train_data,valid_data,alpha,epochs,batch_size)
 
